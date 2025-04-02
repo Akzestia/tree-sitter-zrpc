d9d8ad8bf243f7920c37c74d2fca2056df3a32b5
アクゼスティア <akzestia@gmail.com>
2025-04-02 17:34:18 +0200

    Highlights update:

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,16 +1,39 @@
-
+; Keywords
 (scheme_definition
   "scheme" @keyword)
+(route_definition
+  "route" @keyword)
+(request_block
+  "request" @keyword)
+(response_block
+  "response" @keyword)
+(import_statement
+  "import" @include)
 
-(scheme_name) @type.definition
+; Types
+(struct_name) @type.definition
+(user_defined_type) @type
+(scheme_type) @type.builtin
 
+; Variables
 (field_name) @variable
+(identifier) @variable
 
-(scheme_type) @type
+; Strings
+(string_literal) @string
 
-(block
+; Punctuation
+(scheme_block
   "{" @punctuation.bracket
   "}" @punctuation.bracket)
-
+(route_block
+  "{" @punctuation.bracket
+  "}" @punctuation.bracket)
+(request_block
+  "{" @punctuation.bracket
+  "}" @punctuation.bracket)
+(response_block
+  "{" @punctuation.bracket
+  "}" @punctuation.bracket)
 (_statement
   ":" @punctuation.delimiter)
